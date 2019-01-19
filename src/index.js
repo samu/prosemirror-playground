b77b7eefb4a5dd8a486c1644a1d3a78568eb098c
samu <mueller.samu@gmail.com>
2019-01-19 12:58:51 +0100

    custom join

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
-import { Schema, DOMParser } from "prosemirror-model";
+import { Schema, DOMParser, Fragment, Slice } from "prosemirror-model";
 import { schema } from "prosemirror-schema-basic";
 import { addListNodes } from "prosemirror-schema-list";
 import { exampleSetup } from "prosemirror-example-setup";
 import { keymap } from "prosemirror-keymap";
 import "prosemirror-menu/../style/menu.css";
-import { liftTarget } from "prosemirror-transform";
+import { liftTarget, ReplaceAroundStep } from "prosemirror-transform";
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
@@ -20,6 +20,28 @@ const mySchema = new Schema({
   ),
   marks: schema.spec.marks
 });
+
+// prettier-ignore
+function sinkListItem(itemType) {
+  return function(tr, $pos) {
+    let range = $pos.blockRange($pos, node => node.childCount && node.firstChild.type == itemType)
+    if (!range) return false
+    let startIndex = range.startIndex
+    if (startIndex == 0) return false
+    let parent = range.parent, nodeBefore = parent.child(startIndex - 1)
+    if (nodeBefore.type != itemType) return false
+
+    let nestedBefore = nodeBefore.lastChild && nodeBefore.lastChild.type == parent.type
+    let inner = Fragment.from(nestedBefore ? itemType.create() : null)
+    let slice = new Slice(Fragment.from(itemType.create(null, Fragment.from(parent.copy(inner)))),
+                          nestedBefore ? 3 : 1, 0)
+    let before = range.start, after = range.end
+    return tr.step(new ReplaceAroundStep(before - (nestedBefore ? 3 : 1), after,
+                                                 before, after, slice, 1, true))
+  }
+}
+
+const sinker = sinkListItem(mySchema.nodes.list_item);
 
 const myCoolCommand = (state, dispatch) => {
   let { $cursor } = state.selection;
@@ -35,10 +57,10 @@ const myCoolCommand = (state, dispatch) => {
 
   const atStartOfParentNode = $cursor.index($cursor.depth - 1) === 0;
 
+  const insideFirstItemOfList = $cursor.index($cursor.depth - 2) === 0;
+
   if (atStartOfTextNode && atStartOfParentNode && insideListNode) {
     if (dispatch) {
-      const insideFirstItemOfList = $cursor.index($cursor.depth - 2) === 0;
-
       const insideAnotherList =
         $cursor.node($cursor.depth - 3).type.name === "list_item";
 
@@ -52,6 +74,16 @@ const myCoolCommand = (state, dispatch) => {
       } else {
         tr = tr.join(tr.mapping.map($cursor.pos - 2));
         tr = tr.join(tr.mapping.map($cursor.pos - 2));
+
+        // let newState;
+        //
+        // tr = sinker(tr, $cursor);
+        //
+        // newState = state.apply(tr);
+        // tr = sinker(tr, newState.doc.resolve(tr.mapping.map($cursor.pos)));
+        //
+        // tr = tr.join(tr.mapping.map($cursor.pos - 2));
+        // tr = tr.join(tr.mapping.map($cursor.pos - 2));
       }
 
       tr = tr.scrollIntoView();
